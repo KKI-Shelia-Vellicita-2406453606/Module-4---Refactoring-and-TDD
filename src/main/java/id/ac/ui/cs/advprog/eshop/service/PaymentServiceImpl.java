78e767795e5a2e223a12e3fa89ce4c75bd39e724
kerangleeya <shelia.vellicita13@gmail.com>
2026-03-10 14:21:09 +0700

    [GREEN] Implement PaymentService logic

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -0,0 +1,44 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Override
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        Payment payment = new Payment(order, method, paymentData);
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public Payment setStatus(Payment payment, String status) {
+        payment.setStatus(status);
+        return paymentRepository.save(payment);
+    }
+
+    @Override
+    public Payment getPayment(String paymentId) {
+        Payment payment = paymentRepository.findById(paymentId);
+        if (payment == null) {
+            throw new NoSuchElementException("Payment not found");
+        }
+        return payment;
+    }
+
+    @Override
+    public List<Payment> getAllPayment() {
+        return paymentRepository.findAll();
+    }
+}
