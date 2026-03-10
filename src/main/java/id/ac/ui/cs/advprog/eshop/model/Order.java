96eb38e7e582c95aa6d4512b9f439001fddfaea9
kerangleeya <shelia.vellicita13@gmail.com>
2026-03-10 13:03:35 +0700

    fix: PMD violation in Order constructor

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -30,7 +30,12 @@ public class Order {
 
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
         this(id, products, orderTime, author);
-        this.setStatus(status);
+
+        if (OrderStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public void setStatus(String status) {
