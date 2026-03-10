3ca0a58daa57fa38062dbdc5fbdc2c2e3900da54
kerangleeya <shelia.vellicita13@gmail.com>
2026-03-10 14:09:53 +0700

    [RED] Edit test for Payment repository

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,7 +19,15 @@ class PaymentRepositoryTest {
     @BeforeEach
     void setUp() {
         paymentRepository = new PaymentRepository();
-        Order order = new Order("order-1", new ArrayList<>(), 123456789L, "Author");
+
+        List<Product> products = new ArrayList<>();
+        Product product = new Product();
+        product.setProductId("p-1");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(2);
+        products.add(product);
+
+        Order order = new Order("order-1", products, 123456789L, "Author");
         payment = new Payment(order, "Voucher Code", new HashMap<>());
     }
 
